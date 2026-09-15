9352f9e03d2ca42de35c519a38bdf7fc4fcd1ae0
Serebrovsky Sergey Maksimovich. <61110689+Mika-dot@users.noreply.github.com>
2026-09-15 09:35:03 +0300

    chore: launch Warehouse union aggregator v3.4.2

--- deploy/RUN_AGGREGATOR_V3.cmd
+++ deploy/RUN_AGGREGATOR_V3.cmd
@@ -1,24 +1,23 @@
 @echo off
 setlocal EnableExtensions DisableDelayedExpansion
-title 4 KPP Aggregator FINAL FIXED v3.4.1 Warehouse
+title 4 KPP Aggregator FINAL FIXED v3.4.2 Warehouse Union
 cd /d "%~dp0.."
 set "ROOT=%CD%"
 
 if not exist "%ROOT%\deploy\config_v3.cmd" goto :missing_config
 call "%ROOT%\deploy\config_v3.cmd"
 if errorlevel 1 goto :fatal
-
 if not defined PY64 call "%ROOT%\deploy\resolve_python.cmd"
 if errorlevel 1 goto :fatal
 if not defined PY64 goto :missing_python
 if not exist "%PY64%" goto :bad_python
-if not exist "%ROOT%\KPP\kpp_aggregator_v3_warehouse.py" goto :bad_script
+if not exist "%ROOT%\KPP\kpp_aggregator_v3_warehouse_v2.py" goto :bad_script
 
 cd /d "%ROOT%\KPP"
 if errorlevel 1 goto :bad_workdir
 
 :restart
-"%PY64%" -u "%ROOT%\KPP\kpp_aggregator_v3_warehouse.py"
+"%PY64%" -u "%ROOT%\KPP\kpp_aggregator_v3_warehouse_v2.py"
 set "RC=%ERRORLEVEL%"
 echo.
 echo [%DATE% %TIME%] Exit code %RC%. Restarting in 5 seconds.
@@ -34,11 +33,7 @@ echo [FATAL] PY64 is empty after Python detection.
 goto :fatal
 
 :bad_python
-echo [FATAL] Python file does not exist: %PY64%
-goto :fatal
-
-:bad_script
-echo [FATAL] Script does not exist: %ROOT%\KPP\kpp_aggregator_v3_warehouse.py
+echo [FATAL] Python file does not exist: %ROOT%\KPP\kpp_aggregator_v3_warehouse_v2.py
 goto :fatal
 
 :bad_workdir
