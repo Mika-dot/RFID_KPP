f105c9461a4704b79976f816d0d1d4d39fee2bdf
Serebrovsky Sergey Maksimovich. <61110689+Mika-dot@users.noreply.github.com>
2026-09-15 09:35:12 +0300

    chore: launch Warehouse direct-union web report v3.4.2

--- deploy/RUN_WEB_V3.cmd
+++ deploy/RUN_WEB_V3.cmd
@@ -1,13 +1,12 @@
 @echo off
 setlocal EnableExtensions DisableDelayedExpansion
-title 5 RFID KPP Web FINAL FIXED v3.4.1 Warehouse Report
+title 5 RFID KPP Web FINAL FIXED v3.4.2 Warehouse Union Report
 cd /d "%~dp0.."
 set "ROOT=%CD%"
 
 if not exist "%ROOT%\deploy\config_v3.cmd" goto :missing_config
 call "%ROOT%\deploy\config_v3.cmd"
 if errorlevel 1 goto :fatal
-
 if not defined PY64 call "%ROOT%\deploy\resolve_python.cmd"
 if errorlevel 1 goto :fatal
 if not defined PY64 goto :missing_python
@@ -34,11 +33,7 @@ echo [FATAL] PY64 is empty after Python detection.
 goto :fatal
 
 :bad_python
-echo [FATAL] Python file does not exist: %PY64%
-goto :fatal
-
-:bad_script
-echo [FATAL] Script does not exist: %ROOT%\web\kpp_reel_dashboard_v3_fixed.py
+echo [FATAL] Python file does not exist: %ROOT%\web\kpp_reel_dashboard_v3_fixed.py
 goto :fatal
 
 :bad_workdir
